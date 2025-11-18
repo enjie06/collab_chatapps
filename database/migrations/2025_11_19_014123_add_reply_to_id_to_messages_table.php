39b36d6cca588cb92b777d1033f57b85d0a33c54
enjie06 <anggie.rahmadina904@gmail.com>
2025-11-19 02:00:19 +0700

    Create 2025_11_19_014123_add_reply_to_id_to_messages_table.php

---
+++ database/migrations/2025_11_19_014123_add_reply_to_id_to_messages_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->foreignId('reply_to_id')->nullable()->constrained('messages')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->dropForeign(['reply_to_id']);
+            $table->dropColumn('reply_to_id');
+        });
+    }
+};
