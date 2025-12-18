80a03bba70a0cdc0f14bae137d0413e1bffc8769
Vina-permatasari <vinapermatasari670@gmail.com>
2025-12-19 06:02:21 +0700

    Up navbar dikit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,16 +4,28 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                <div class="shrink-0 flex items-center gap-2">
+                    <a href="{{ route('dashboard') }}" class="flex items-center gap-2">
+                        <img
+                            src="{{ asset('images/logo fluffy.jpg') }}"
+                            alt="Fluffy Logo"
+                            class="h-12 w-12 object-contain"
+                        >
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+               <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link
+                        :href="route('dashboard')"
+                        :active="request()->routeIs('dashboard')"
+                        style="
+                            font-family: 'Times New Roman', Times, serif;
+                            font-weight: 600;
+                            font-size: 18px;
+                        "
+                    >
+                        {{ __('Fluffy') }}
                     </x-nav-link>
                 </div>
             </div>
